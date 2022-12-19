4ad350f625be6f14ef27bd7fa2d96edb9b3fe1da
devferx <ferkki@hotmail.com>
2022-12-19 00:36:32 -0400

    Visualización de Caminos Aleatorios

--- 02_camino_borracho/__init__.py
+++ 02_camino_borracho/__init__.py
@@ -1,4 +1,5 @@
 from typing import List
+from bokeh.plotting import figure, show
 from borracho import BorrachoTradicional, Borracho
 from campo import Campo
 from coordenada import Coordenada
@@ -31,21 +32,38 @@ def simular_caminita(
     return distancias
 
 
+def graficar(x, y):
+    grafica = figure(
+        title="Camino aleatorio",
+        x_axis_label="pasos",
+        y_axis_label="distancia",
+    )
+    grafica.line(x, y, legend_label="distancia media")
+
+    show(grafica)
+
+
 def main(
     distancias_de_caminata: List[int],
     numero_de_intentos: int,
     tipo_de_borracho: Borracho,
 ):
+    distancias_media_por_caminata = []
+
     for pasos in distancias_de_caminata:
         distancias = simular_caminita(pasos, numero_de_intentos, tipo_de_borracho)
         distancia_media = round(sum(distancias) / len(distancias), 4)
         distancia_maxima = max(distancias)
         distancia_minima = min(distancias)
 
+        distancias_media_por_caminata.append(distancia_media)
+
         print(f"{tipo_de_borracho.__name__} caminata aleatoria de {pasos} pasos")
         print(f"Media = {distancia_media}")
         print(f"Máxima = {distancia_maxima}")
         print(f"Mínima = {distancia_minima}")
+
+    graficar(distancias_de_caminata, distancias_media_por_caminata)
 
 
 if __name__ == "__main__":
