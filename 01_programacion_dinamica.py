d8ea2745a6ad18f9cd1c67cf9e520f0475a6b524
devferx <ferkki@hotmail.com>
2022-12-18 23:33:54 -0400

    Optimización de Fibonacci

--- 01_programacion_dinamica.py
+++ 01_programacion_dinamica.py
@@ -1,2 +1,32 @@
+import sys
+
+
+def fibonacci_recursivo(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2)
+
+
+def fibonaccio_dinamico(n, memo={}):
+    if n == 0 or n == 1:
+        return 1
+
+    try:
+        return memo[n]
+    except KeyError:
+        resultado = fibonaccio_dinamico(n - 1, memo) + fibonaccio_dinamico(n - 2, memo)
+        memo[n] = resultado
+
+        return resultado
+
+
 def main():
-    pass
+    sys.setrecursionlimit(100002)
+    n = int(input("Escoge un numero: "))
+    resultado = fibonaccio_dinamico(n)
+    print(resultado)
+
+
+if __name__ == "__main__":
+    main()
